49c81a55add6b50d28ba7e464cbdb170b75c1551
Qiugu He <kaochi545121151@gmail.com>
2020-06-23 13:57:35 -0500

    set up basic route

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,36 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-require('dotdev').congic()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//routes
+const blogRoutes = require('./routes/blog')
 
 //app
 const app = express()
+
+//db
+mongoose
+    .connect(process.env.DATABASE, {useNewUrlParser:true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
+    .then(()=> console.log('DB connected'));
 
 //middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cirs())
 
-//router
-app.get('/api', (req, res) =>{
-    res.json({time: Data().toString()})
-})
+//cors
+if(process.env.NODE_ENV === 'development'){
+    app.use(cors({origin: '${process.env.CLIENT_URL}'}));
+};
+
+//routes middleware
+app.use('/api', blogRoutes);
+
+
+//port 
+const port = process.env.PORT || 8000
+app.listen(port, () =>{
+    console.log('Server is running on port '+ port);
+});
