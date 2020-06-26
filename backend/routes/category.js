eacffe83c3dc5731351da9089470f5fa07ad2de2
Qiugu He <kaochi545121151@gmail.com>
2020-06-26 12:17:34 -0500

    Added methods for create, list, read, remove category/categories

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/category');
+const { create, list, read, remove } = require('../controllers/category');
 
 // validators
 const { runValidation } = require('../validators');
@@ -9,6 +9,9 @@ const { requireSignin,adminMiddleware } = require('../controllers/auth');
 
 
 router.post('/category',categoryCreateValidator,runValidation, requireSignin, adminMiddleware, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
 
 
 module.exports = router;
