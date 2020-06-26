15e9cb69f1f7d05e307676cf1cd77fa82349ac8c
Qiugu He <kaochi545121151@gmail.com>
2020-06-26 14:12:07 -0500

    Enable showing, create,  delete categories from backend  to frontend

--- frontend/components/crud/Category.js
+++ frontend/components/crud/Category.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
-import { isAuth, getCookie } from '../../actions/auth';
-import { create } from '../../actions/category';
+import { getCookie } from '../../actions/auth';
+import { create, getCategories, removeCategory } from '../../actions/category';
 
 const Category = () => {
     const [values, setValues] = useState({
@@ -10,11 +10,59 @@ const Category = () => {
         error: false,
         success: false,
         categories: [],
-        removed: false
+        removed: false,
+        reload: false
     });
 
-    const { name, error, success, categories, removed } = values;
+    const { name, error, success, categories, removed, reload } = values;
     const token = getCookie('token');
+
+    useEffect(() => {
+        loadCategories();
+    }, [reload]);
+
+    const loadCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setValues({ ...values, categories: data });
+            }
+        });
+    };
+
+    const showCategories = () => {
+        return categories.map((c, i) => {
+            return (
+                <button
+                    onDoubleClick={() => deleteConfirm(c.slug)}
+                    title="Double click to delete"
+                    key={i}
+                    className="btn btn-outline-primary mr-1 ml-1 mt-3"
+                >
+                    {c.name}
+                </button>
+            );
+        });
+    };
+
+    const deleteConfirm = slug => {
+        let answer = window.confirm('Are you sure you want to delete this category?');
+        if (answer) {
+            deleteCategory(slug);
+        }
+    };
+
+    const deleteCategory = slug => {
+        // console.log('delete', slug);
+        removeCategory(slug, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setValues({ ...values, error: false, success: false, name: '', removed: !removed, reload: !reload });
+            }
+        });
+    };
 
     const clickSubmit = e => {
         e.preventDefault();
@@ -23,13 +71,35 @@ const Category = () => {
             if (data.error) {
                 setValues({ ...values, error: data.error, success: false });
             } else {
-                setValues({ ...values, error: false, success: true, name: '' });
+                setValues({ ...values, error: false, success: false, name: '', removed: !removed, reload: !reload });
             }
         });
     };
 
     const handleChange = e => {
         setValues({ ...values, name: e.target.value, error: false, success: false, removed: '' });
+    };
+
+    const showSuccess = () => {
+        if (success) {
+            return <p className="text-success">Category is created</p>;
+        }
+    };
+
+    const showError = () => {
+        if (error) {
+            return <p className="text-danger">Category already exist</p>;
+        }
+    };
+
+    const showRemoved = () => {
+        if (removed) {
+            return <p className="text-danger">Category is removed</p>;
+        }
+    };
+
+    const mouseMoveHandler = e => {
+        setValues({ ...values, error: false, success: false, removed: '' });
     };
 
     const newCategoryFom = () => (
@@ -46,7 +116,17 @@ const Category = () => {
         </form>
     );
 
-    return <React.Fragment>{newCategoryFom()}</React.Fragment>;
+    return (
+        <React.Fragment>
+            {showSuccess()}
+            {showError()}
+            {showRemoved()}
+            <div onMouseMove={mouseMoveHandler}>
+                {newCategoryFom()}
+                {showCategories()}
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Category;
