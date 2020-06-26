a3ba736d01b80fcc261f9f8ac529f6baa3182309
Qiugu He <kaochi545121151@gmail.com>
2020-06-26 11:55:08 -0500

    Created category schema, validator, routes and controller

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
 
 //app
 const app = express()
@@ -33,6 +34,7 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
 
 
 
