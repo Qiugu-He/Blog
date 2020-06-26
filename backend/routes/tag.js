57f8b00a39a731f6f4f581ad70d37cbc39e4d6ae
Qiugu He <kaochi545121151@gmail.com>
2020-06-26 12:32:54 -0500

    Created Tag module, added methods of create, list, read, remove for tag

---
+++ backend/routes/tag.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// validators
+const { runValidation } = require('../validators');
+const { createTagValidator } = require('../validators/tag');
+
+//controller
+const { requireSignin,adminMiddleware } = require('../controllers/auth');
+const { create, list, read, remove } = require('../controllers/tag');
+
+
+router.post('/tag',createTagValidator,runValidation, requireSignin, adminMiddleware, create);
+router.get('/tags', list);
+router.get('/tag/:slug', read);
+router.delete('/tag/:slug', requireSignin, adminMiddleware, remove);
+
+
+module.exports = router;
