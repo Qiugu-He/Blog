efdbb8c4b3ade2a2dea079c9a17af8848755ec98
Qiugu He <kaochi545121151@gmail.com>
2020-06-26 22:23:21 -0500

    enable showing categories and tags at right in checkbox

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -23,6 +23,9 @@ const CreateBlog = ({ router }) => {
         }
     };
 
+    const [categories, setCategories] = useState([]);
+    const [tags, setTags] = useState([]);
+
     const [body, setBody] = useState(blogFromLS());
     const [values, setValues] = useState({
         error: '',
@@ -37,7 +40,29 @@ const CreateBlog = ({ router }) => {
 
     useEffect(() => {
         setValues({ ...values, formData: new FormData() });
+        initCategories();
+        initTags();
     }, [router]);
+
+    const initCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setCategories(data);
+            }
+        });
+    };
+
+    const initTags = () => {
+        getTags().then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setTags(data);
+            }
+        });
+    };
 
     const publishBlog = e => {
         e.preventDefault();
@@ -58,6 +83,30 @@ const CreateBlog = ({ router }) => {
         if (typeof window !== 'undefined') {
             localStorage.setItem('blog', JSON.stringify(e));
         }
+    };
+
+    const showCategories = () => {
+        return (
+            categories &&
+            categories.map((c, i) => (
+                <li key={i} className="list-unstyled">
+                    <input type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{c.name}</label>
+                </li>
+            ))
+        );
+    };
+
+    const showTags = () => {
+        return (
+            tags &&
+            tags.map((t, i) => (
+                <li key={i} className="list-unstyled">
+                    <input type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{t.name}</label>
+                </li>
+            ))
+        );
     };
 
     const createBlogForm = () => {
@@ -88,12 +137,34 @@ const CreateBlog = ({ router }) => {
     };
 
     return (
-        <div>
-            {createBlogForm()}
-            <hr />
-            {JSON.stringify(title)}
-            <hr />
-            {JSON.stringify(body)}
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-8">
+                    {createBlogForm()}
+                    <hr />
+                    {JSON.stringify(title)}
+                    <hr />
+                    {JSON.stringify(body)}
+                    <hr />
+                    {JSON.stringify(categories)}
+                    <hr />
+                    {JSON.stringify(tags)}
+                </div>
+
+                <div className="col-md-4">
+                    <div>
+                        <h5>Categories</h5>
+                        <hr />
+
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showCategories()}</ul>
+                    </div>
+                    <div>
+                        <h5>Tags</h5>
+                        <hr />
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showTags()}</ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
@@ -127,4 +198,4 @@ CreateBlog.formats = [
     'code-block'
 ];
 
-export default withRouter(CreateBlog);
+export default withRouter(CreateBlog)
