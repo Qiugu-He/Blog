815a33188e2c886d6039f9e9637588f67e80ba79
Qiugu He <kaochi545121151@gmail.com>
2020-06-29 15:30:39 -0500

    Added related blog section of a blog in frontend

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -286,7 +286,8 @@ exports.listRelated = (req, res) => {
                 return res.status(400).json({
                     error: 'Blogs not found'
                 });
-                res.json(blogs);
             }
+
+            res.json(blogs);
         });
 };
