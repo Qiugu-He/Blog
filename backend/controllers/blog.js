ea2885d9838c225584ab3cac4f79519a6431765c
Qiugu He <kaochi545121151@gmail.com>
2020-06-28 22:37:07 -0500

    Enabled listing blogs with its categories and tags

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -102,3 +102,67 @@ exports.create = (req, res) =>{
         });
     });
 };
+
+
+//list, listAllBlogsCategoriesTags, read, remove, update 
+exports.list = (req, res) => {
+    Blog.find({})
+        .populate('categories', '_id name slug')
+        .populate('tags', '_id name slug')
+        .populate('postedBy', '_id name username')
+        .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+        .exec((err, data) => {
+            if (err) {
+                return res.json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(data);
+        });
+};
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    let blogs;
+    let categories;
+    let tags;
+
+    Blog.find({})
+        .populate('categories', '_id name slug')
+        .populate('tags', '_id name slug')
+        .populate('postedBy', '_id name username profile')
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+        .exec((err, data) => {
+            if (err) {
+                return res.json({
+                    error: errorHandler(err)
+                });
+            }
+            blogs = data; // blogs
+            // get all categories
+            Category.find({}).exec((err, c) => {
+                if (err) {
+                    return res.json({
+                        error: errorHandler(err)
+                    });
+                }
+                categories = c; // categories
+                // get all tags
+                Tag.find({}).exec((err, t) => {
+                    if (err) {
+                        return res.json({
+                            error: errorHandler(err)
+                        });
+                    }
+                    tags = t;
+                    // return all blogs categories tags
+                    res.json({ blogs, categories, tags, size: blogs.length });
+                });
+            });
+        });
+};
