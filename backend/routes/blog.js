9e0c058a94637a7b7aadc092f336be48ea48e8f6
Qiugu He <kaochi545121151@gmail.com>
2020-06-28 22:48:51 -0500

    Enabled read and detele blog in backend

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -8,8 +8,8 @@ const { requireSignin, adminMiddleware } = require('../controllers/auth');
 router.post('/blog', requireSignin, adminMiddleware, create);
 router.get('/blogs', list);
 router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
-// router.get('/blog/:slug', read);
-// router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
 // router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 
 module.exports = router;
