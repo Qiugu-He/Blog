e17adc1d68fcbcef8a3551067197b0de87f4f694
Qiugu He <kaochi545121151@gmail.com>
2020-06-29 12:10:36 -0500

    Enable showing categories and tags of a blog

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -6,42 +6,15 @@ import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
 import { API } from '../../config';
 import renderHTML from 'react-render-html';
 import moment from 'moment';
+import Card from '../../components/blog/Card';
 
 const Blogs = ({ blogs, categories, tags, size }) => {
     const showAllBlogs = () => {
         return blogs.map((blog, i) => {
-            // ()
+ 
             return (
                 <article key={i}>
-                    <div className="lead pb-4">
-                        <header>
-                            <Link href={`/blogs/${blog.slug}`}>
-                                <a>
-                                    <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
-                                </a>
-                            </Link>
-                        </header>
-                        <section>
-                            <p className="mark ml-1 pt-2 pb-2">
-                                Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
-                            </p>
-                        </section>
-                        <section>
-                            <p>blog categories and tags</p>
-                        </section>
-
-                        <div className="row">
-                            <div className="col-md-4">image</div>
-                            <div className="col-md-8">
-                                <section>
-                                    <div className="pb-3">{renderHTML(blog.excerpt)}</div>
-                                    <Link href={`/blogs/${blog.slug}`}>
-                                        <a className="btn btn-primary pt-2">Read more</a>
-                                    </Link>
-                                </section>
-                            </div>
-                        </div>
-                    </div>
+                    <Card blog={blog}/>
                     <hr />
                 </article>
             );
