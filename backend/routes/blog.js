14e92423140aa8e5a19234de31a30bdd7cb995de
Qiugu He <kaochi545121151@gmail.com>
2020-06-30 18:20:54 -0500

    Update blog.js

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { create, list, listAllBlogsCategoriesTags, read, remove, update, photo, listRelated, listSearch} = require('../controllers/blog');
+const {
+    create,
+    list,
+    listAllBlogsCategoriesTags,
+    read,
+    remove,
+    update,
+    photo,
+    listRelated,
+    listSearch
+} = require('../controllers/blog');
 
-const { requireSignin, adminMiddleware } = require('../controllers/auth');
+const { requireSignin, adminMiddleware, authMiddleware } = require('../controllers/auth');
 
 router.post('/blog', requireSignin, adminMiddleware, create);
 router.get('/blogs', list);
@@ -13,5 +23,10 @@ router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 router.get('/blog/photo/:slug', photo);
 router.post('/blogs/related', listRelated);
 router.get('/blogs/search', listSearch);
+
+// auth user blog crud
+router.post('/user/blog', requireSignin, authMiddleware, create);
+router.delete('/user/blog/:slug', requireSignin, authMiddleware, remove);
+router.put('/user/blog/:slug', requireSignin, authMiddleware, update);
 
 module.exports = router;
