657324ecaf2dcf1cc669eba4b00c263270c62720
Qiugu He <kaochi545121151@gmail.com>
2020-06-29 20:53:11 -0500

    Updated user profile public page

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -33,6 +33,8 @@ exports.publicProfile = (req, res) => {
                     });
                 }
                 user.photo = undefined;
+                user.hashed_password = undefined;
+                
                 res.json({
                     user,
                     blogs: data
